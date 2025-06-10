ac0960c511b91308127f7ee7fa21a0a1192b764f
swaggiashri@gmail.com <swaggiashri@gmail.com>
2025-06-10 10:40:20 +0530

    adding GFG1.java file

---
+++ GFG1.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class GFG1 {
+    static double area(double h, double b)
+    {
+         return (h * b) / 2;
+    }
+    public static void main(String[] args)
+    {
+        double h = 10;
+        double b = 5;
+        System.out.println("Area of the triangle: "
+                           + area(h, b));
+    }
+}
